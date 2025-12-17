a540c7e11786038269e26ff97b911242e31054d8
Existent_Chicken <isaiahpaultrue@gmail.com>
2025-12-17 15:31:52 -0800

    Initial files for MakeCode project

---
+++ colorAdvanced.ts
@@ -0,0 +1,22 @@
+let i: number
+let paletteDictionary: any = {}
+let paleet: Array<number>
+
+//% block="Color-Advanced"
+//% color=#003300
+//% icon="\uf043"
+
+
+namespace advancedColor {
+    //% block="Add palette with colors $color1 $color2 $color3 $color4 $color5 $color6 $color7 $color8 $color9 $color10 $color11 $color12 $color13 $color14 $color15 with name $name"
+    export function addPalette(color1: color, color2: color, color3: color, color4: color, color5: color, color6: color, color7: color, color8: color, color9: color, color10: color, color11: color, color12: color, color13: color, color14: color, color15: color, name: string){
+        paletteDictionary[name] = [color1,color2,color3,color4,color5,color6,color7,color8,color9,color10,color11,color12,color13,color14,color15]
+    }
+    //% block="Load palette with name $name"
+    export function loadPalette(name: string){
+        paleet = paletteDictionary[name]
+        for(i=1;i<=15;i++){
+            color.setColor(i,paleet[i])
+        }
+    }
+}
---
+++ images.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Carl":return img`
+. . f f f f . . 
+. f 5 5 5 5 f . 
+f 4 f 5 5 f 5 f 
+f 4 5 5 5 1 5 f 
+f 4 5 f f 5 5 f 
+f 4 4 5 5 5 5 f 
+. f 4 4 4 4 f . 
+. . f f f f . . 
+`;
+            case "image2":
+            case "pink":return img`
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
